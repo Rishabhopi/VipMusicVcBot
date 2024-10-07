bc359c6564b76448f1ec4ac194ec95c997fd51aa
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-10-07 10:11:44 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -55,6 +55,6 @@ for filename in os.listdir(r"./strings/langs/"):
         languages_present[language_name] = languages[language_name]["name"]
     except:
         print(
-            "There is some issue with the language file inside bot. Please report it to the TheTeamvk at @TheTeamvk on Telegram"
+            "There is some issue with the language file inside bot. Please report it to the Rishu Support at @ur_support07 on Telegram"
         )
         sys.exit()
